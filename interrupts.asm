361dec304443d1106c1a7011e7f3d3aade7b427f
prometheus :) <44480063+prometheus1337@users.noreply.github.com>
2019-05-09 21:40:55 +0000

    ; not finished yet

---
+++ interrupts.asm
@@ -0,0 +1,59 @@
+global ir0
+global ir1
+global ir2
+global ir3
+global ir4
+global ir5
+global ir6
+global ir7
+global ir8
+global ir9
+global ir10
+global ir11
+global ir12
+global ir13
+global ir14
+global ir15
+global loadirs
+global irh0
+global irh1
+global irh2
+global ir3
+global irh4
+global irh5
+global irh6
+global irh7
+global irh8
+global irh9
+global irh10
+global irh11
+global irh12
+global irh13
+global irh14
+global irh15
+extern irh0
+extern irh1
+extern irh2
+extern ir3
+extern irh4
+extern irh5
+extern irh6
+extern irh7
+extern irh8
+extern irh9
+extern irh10
+extern irh11
+extern irh12
+extern irh13
+extern irh14
+extern irh15
+
+ir0:
+pusha
+call irh0
+popa
+iret
+
+
+
+; not finished yet 
